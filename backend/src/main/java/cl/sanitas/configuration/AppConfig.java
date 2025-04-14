62adf9663243b22e30df940f7dc3ff30e6eae7a4
wlrojas <wl.rojas@duocuc.cl>
2025-04-14 00:16:50 -0400

    feat(beans): added usecase beans

--- backend/src/main/java/cl/sanitas/configuration/AppConfig.java
+++ backend/src/main/java/cl/sanitas/configuration/AppConfig.java
@@ -1,9 +1,13 @@
 package cl.sanitas.configuration;
 
 import cl.sanitas.application.port.CalificacionRepository;
+import cl.sanitas.application.port.CitasRepository;
+import cl.sanitas.application.port.EspecialistaRepository;
 import cl.sanitas.application.port.UsuarioRepository;
+import cl.sanitas.application.usecase.BuscarEspecialistaUseCase;
 import cl.sanitas.application.usecase.BuscarUsuarioUseCase;
 import cl.sanitas.application.usecase.CalificacionUseCase;
+import cl.sanitas.application.usecase.CitasUseCase;
 import cl.sanitas.application.usecase.CrearUsuarioUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,5 +28,15 @@ public class AppConfig {
     @Bean
     public CalificacionUseCase calificacionUseCase(CalificacionRepository calificacionRepository) {
         return new CalificacionUseCase(calificacionRepository);
+    }
+
+    @Bean
+    public BuscarEspecialistaUseCase buscarEspecialistaUseCase(EspecialistaRepository especialistaRepository) {
+        return new BuscarEspecialistaUseCase(especialistaRepository);
+    }
+
+    @Bean
+    public CitasUseCase citasUseCase(CitasRepository citasRepository) {
+        return new CitasUseCase(citasRepository);
     }
 }
