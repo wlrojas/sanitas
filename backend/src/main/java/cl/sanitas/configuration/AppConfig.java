f2b37e9e9fd3ba9211468af15f5e41b5c3fa1b1e
wlrojas <wl.rojas@duocuc.cl>
2025-04-15 00:48:06 -0400

    feat(beans): added new beans for use cases

--- backend/src/main/java/cl/sanitas/configuration/AppConfig.java
+++ backend/src/main/java/cl/sanitas/configuration/AppConfig.java
@@ -3,12 +3,14 @@ package cl.sanitas.configuration;
 import cl.sanitas.application.port.CalificacionRepository;
 import cl.sanitas.application.port.CitasRepository;
 import cl.sanitas.application.port.EspecialistaRepository;
+import cl.sanitas.application.port.HistorialMedicoRepository;
 import cl.sanitas.application.port.UsuarioRepository;
 import cl.sanitas.application.usecase.BuscarEspecialistaUseCase;
 import cl.sanitas.application.usecase.BuscarUsuarioUseCase;
 import cl.sanitas.application.usecase.CalificacionUseCase;
 import cl.sanitas.application.usecase.CitasUseCase;
 import cl.sanitas.application.usecase.CrearUsuarioUseCase;
+import cl.sanitas.application.usecase.HistorialMedicoUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,5 +40,10 @@ public class AppConfig {
     @Bean
     public CitasUseCase citasUseCase(CitasRepository citasRepository) {
         return new CitasUseCase(citasRepository);
+    }
+
+    @Bean
+    public HistorialMedicoUseCase historialMedicoUseCase(HistorialMedicoRepository historialRepository) {
+        return new HistorialMedicoUseCase(historialRepository);
     }
 }
