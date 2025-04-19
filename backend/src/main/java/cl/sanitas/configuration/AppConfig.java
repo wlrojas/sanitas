90d2325b75f0fd55b9797f66053eeb571facf3f3
wlrojas <wl.rojas@duocuc.cl>
2025-04-19 19:29:50 -0400

    feat(security): becrypt added to encrypt password

--- backend/src/main/java/cl/sanitas/configuration/AppConfig.java
+++ backend/src/main/java/cl/sanitas/configuration/AppConfig.java
@@ -1,49 +1,15 @@
 package cl.sanitas.configuration;
 
-import cl.sanitas.application.port.CalificacionRepository;
-import cl.sanitas.application.port.CitasRepository;
-import cl.sanitas.application.port.EspecialistaRepository;
-import cl.sanitas.application.port.HistorialMedicoRepository;
-import cl.sanitas.application.port.UsuarioRepository;
-import cl.sanitas.application.usecase.BuscarEspecialistaUseCase;
-import cl.sanitas.application.usecase.BuscarUsuarioUseCase;
-import cl.sanitas.application.usecase.CalificacionUseCase;
-import cl.sanitas.application.usecase.CitasUseCase;
-import cl.sanitas.application.usecase.CrearUsuarioUseCase;
-import cl.sanitas.application.usecase.HistorialMedicoUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public CrearUsuarioUseCase crearUsuarioUseCase(UsuarioRepository usuarioRepository) {
-        return new CrearUsuarioUseCase(usuarioRepository);
-    }
-
-    @Bean
-    public BuscarUsuarioUseCase buscarUsuarioUseCase(UsuarioRepository usuarioRepository) {
-        return new BuscarUsuarioUseCase(usuarioRepository);
-    }
-
-    @Bean
-    public CalificacionUseCase calificacionUseCase(CalificacionRepository calificacionRepository) {
-        return new CalificacionUseCase(calificacionRepository);
-    }
-
-    @Bean
-    public BuscarEspecialistaUseCase buscarEspecialistaUseCase(EspecialistaRepository especialistaRepository) {
-        return new BuscarEspecialistaUseCase(especialistaRepository);
-    }
-
-    @Bean
-    public CitasUseCase citasUseCase(CitasRepository citasRepository) {
-        return new CitasUseCase(citasRepository);
-    }
-
-    @Bean
-    public HistorialMedicoUseCase historialMedicoUseCase(HistorialMedicoRepository historialRepository) {
-        return new HistorialMedicoUseCase(historialRepository);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
